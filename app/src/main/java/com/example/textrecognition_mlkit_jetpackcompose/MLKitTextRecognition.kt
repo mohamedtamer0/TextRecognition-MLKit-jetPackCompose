475bde6aed23d8cbfc7de2d683546b9862d99899
mohamedtamer0 <mt4452242@gmail.com>
2022-02-04 17:26:31 +0200

    update

--- app/src/main/java/com/example/textrecognition_mlkit_jetpackcompose/MLKitTextRecognition.kt
+++ app/src/main/java/com/example/textrecognition_mlkit_jetpackcompose/MLKitTextRecognition.kt
@@ -1,22 +1,35 @@
 package com.example.textrecognition_mlkit_jetpackcompose
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.TextRecognizer
+import java.util.concurrent.Executors
 
 
 @Composable
@@ -33,7 +46,13 @@ fun MLKitTextRecognition() {
             extractedText = extractedText
         )
 
-        Text(text = extractedText.value, Modifier.fillMaxWidth().background(Color.White).padding(16.dp))
+        Text(
+            text = extractedText.value,
+            Modifier
+                .fillMaxWidth()
+                .background(Color.White)
+                .padding(16.dp)
+        )
     }
 }
 
@@ -44,4 +63,96 @@ fun TextRecognitionView(
     lifecycleOwner: LifecycleOwner,
     extractedText: MutableState<String>
 ) {
+    val cameraProviderFuture = remember { ProcessCameraProvider.getInstance(context) }
+    var preview by remember { mutableStateOf<Preview?>(null) }
+    val executor = ContextCompat.getMainExecutor(context)
+    val cameraProvider = cameraProviderFuture.get()
+    val textRecognizer = remember { TextRecognition.getClient() }
+    val cameraExecutor = remember { Executors.newSingleThreadExecutor() }
+
+
+    Box {
+        AndroidView(modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.7f),
+            factory = { ctx ->
+                val previewView = PreviewView(ctx)
+                cameraProviderFuture.addListener({
+                    val imageAnalysis = ImageAnalysis.Builder()
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                        .build()
+                        .apply {
+                            setAnalyzer(
+                                cameraExecutor,
+                                ObjectDetectorImageAnalyzer(textRecognizer, extractedText)
+                            )
+                        }
+
+                    val cameraSelector = CameraSelector.Builder()
+                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                        .build()
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner,
+                        cameraSelector,
+                        imageAnalysis,
+                        preview
+                    )
+                }, executor)
+                preview = Preview.Builder().build().also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+                previewView
+            }
+        )
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(15.dp)
+                .align(
+                    Alignment.TopStart
+                )
+        ) {
+            IconButton(onClick = {
+                Toast.makeText(context, "Back Clicked", Toast.LENGTH_LONG).show()
+            }) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "back",
+                    tint = Color.White
+                )
+            }
+        }
+    }
 }
+
+
+class ObjectDetectorImageAnalyzer(
+    private val textRecognizer: TextRecognizer,
+    private val extractedText: MutableState<String>
+) : ImageAnalysis.Analyzer {
+    @SuppressLint("UnsafeOptInUsageError")
+    override fun analyze(imageProxy: ImageProxy) {
+        val mediaImage = imageProxy.image
+        if (mediaImage != null) {
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+
+            textRecognizer.process(image)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        extractedText.value = it.result?.text ?: ""
+                    }
+                    imageProxy.close()
+                }
+        }
+    }
+
+}
+
+
+
+
+
+
--- app/src/main/java/com/example/textrecognition_mlkit_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/textrecognition_mlkit_jetpackcompose/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.textrecognition_mlkit_jetpackcompose
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,7 +14,32 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.example.textrecognition_mlkit_jetpackcompose.ui.theme.TextRecognitionMLKitjetPackComposeTheme
+
+private const val REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST = 34
+
+private fun foregroundPermissionApproved(context: Context): Boolean {
+    return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
+        context, Manifest.permission.CAMERA
+    )
+}
+
+private fun requestForegroundPermission(context: Context) {
+    val provideRationale = foregroundPermissionApproved(context)
+
+    if (provideRationale) {
+        ActivityCompat.requestPermissions(
+            context as Activity,
+            arrayOf(Manifest.permission.CAMERA), REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST
+        )
+    } else {
+        ActivityCompat.requestPermissions(
+            context as Activity,
+            arrayOf(Manifest.permission.CAMERA), REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST
+        )
+    }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +51,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TextRecognitionMLKitjetPackComposeTheme {
-        Greeting("Android")
-    }
-}
