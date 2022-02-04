ab7b52f7dcff7418713218071283359896f581ea
mohamedtamer0 <mt4452242@gmail.com>
2022-02-04 16:58:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/textrecognition_mlkit_jetpackcompose/MLKitTextRecognition.kt
@@ -0,0 +1,47 @@
+package com.example.textrecognition_mlkit_jetpackcompose
+
+import android.content.Context
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LifecycleOwner
+
+
+@Composable
+fun MLKitTextRecognition() {
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val extractedText = remember { mutableStateOf("") }
+
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        TextRecognitionView(
+            context = context,
+            lifecycleOwner = lifecycleOwner,
+            extractedText = extractedText
+        )
+
+        Text(text = extractedText.value, Modifier.fillMaxWidth().background(Color.White).padding(16.dp))
+    }
+}
+
+
+@Composable
+fun TextRecognitionView(
+    context: Context,
+    lifecycleOwner: LifecycleOwner,
+    extractedText: MutableState<String>
+) {
+}
